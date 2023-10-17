c01a979604264f4eb84da965c837ab2e1b9a2e6e
szt2otokov <szt2otokov@vasvari.org>
2023-10-17 08:31:12 +0200

    Add project files.

---
+++ IskolaAPI/Controllers/DbController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+namespace IskolaAPI.Controllers
+{
+    [Controller]
+    public class DbController : Controller
+    {
+        public DbController()
+        {
+            try
+            {
+                var db = new MySqlConnection
+                {
+                    ConnectionString = "server=127.0.0.1;database=iskola"
+                };
+                db.Open();
+                Console.WriteLine(db.State);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ IskolaAPI/Program.cs
@@ -0,0 +1,28 @@
+using IskolaAPI.Controllers;
+
+namespace IskolaAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            var dbCon = new DbController();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
